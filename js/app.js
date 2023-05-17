467f029000d4ac1b2f6c422a92e95c1abaf5bdf3
Josh McHugh <joshua.mchugh01@gmail.com>
2023-05-16 23:33:25 -0400

    Added new components

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 class Resume extends React.Component {
 
     state = {
-        resume: {}
-    }
+        resume: []
+    };
 
     componentDidMount() {
         this.loadResume();
@@ -15,19 +15,28 @@ class Resume extends React.Component {
     }
 
     render() {
-        return (<Sheet resume={this.state.resume} />);
+        return (
+            <SheetList sheets={this.state.resume} />
+        );
+    }
+}
+
+class SheetList extends React.Component {
+    render() {
+        return this.props.sheets.map(
+            sheet => <Sheet key={sheet.page} sideContent={sheet.sideContent} />
+        );
     }
 }
 
 class Sheet extends React.Component {
     render() {
-        console.log(this.props);
         return (
             <section className="sheet h-297mm">
               <div className="flex h-full font-['Roboto'] text-gray-700">
 	              <div className="flex-auto w-4 bg-primary-dark text-white">
 	                <div className="mt-14 ml-8">
-	                  Side Conent
+                    <SideContentList sideContent={this.props.sideContent} />
 	                </div>
 	              </div>
 	              <div className="flex-auto w-64 my-16 px-7">
@@ -35,6 +44,28 @@ class Sheet extends React.Component {
                 </div>
 	            </div>
             </section>
+        );
+    }
+}
+
+class SideContentList extends React.Component {
+    render() {
+        return this.props.sideContent.map(content => {
+            if(content.templateId === 'nameTemplate') {
+                return (<NameSection {...content.data} />);
+            }
+            return (<div></div>);
+        });
+    }
+}
+
+class NameSection extends React.Component {
+    render() {
+        return (
+            <div>
+              <div className="font-bold text-3xl">{this.props.name}</div>
+              <div className="text-sm tracking-wider">{this.props.title}</div>
+            </div>
         );
     }
 }
