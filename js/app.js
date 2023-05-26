a4ab74d5fb1477b7002ff71e448aecb795539772
Josh McHugh <joshua.mchugh01@gmail.com>
2023-05-25 23:20:31 -0400

    Cleaning up indentions

--- js/app.js
+++ js/app.js
@@ -35,15 +35,15 @@ class Sheet extends React.Component {
         return (
             <section className="sheet h-297mm">
               <div className="flex h-full font-['Roboto'] text-gray-700">
-	              <div className="flex-auto w-4 bg-primary-dark text-white">
-	                <div className="mt-14 ml-8">
+	        <div className="flex-auto w-4 bg-primary-dark text-white">
+	          <div className="mt-14 ml-8">
                     <SideContentList sideContent={this.props.sideContent} />
-	                </div>
-	              </div>
-	              <div className="flex-auto w-64 my-16 px-7">
+	          </div>
+	        </div>
+	        <div className="flex-auto w-64 my-16 px-7">
                   <MainContentList content={this.props.content} />
                 </div>
-	            </div>
+	      </div>
             </section>
         );
     }
@@ -53,7 +53,7 @@ class SideContentList extends React.Component {
     render() {
         return this.props.sideContent.map((content, index) => {
             if(content.templateId === 'nameTemplate') {
-                return <NameSection key={index} {...content.data} />;
+                return <NameSection key={index} {...content.data} />
             }
             if(content.templateId === 'headerTemplate') {
                 return <HeaderSection key={index} {...content.data} />
@@ -67,7 +67,7 @@ class SideContentList extends React.Component {
             if(content.templateId === 'socialTemplate') {
                 return <SocialSection key={index} data={content.data} />
             }
-            return <div key={index}></div>;
+            return <div key={index}></div>
         });
     }
 }
@@ -76,24 +76,24 @@ class MainContentList extends React.Component {
     render() {
         return this.props.content.map((content, index) => {
             if(content.templateId === 'headerTemplate') {
-                return <HeaderSection key={index} {...content.data} />;
+                return <HeaderSection key={index} {...content.data} />
             }
             if(content.templateId === 'experienceTemplate') {
-                return <ExperienceSection key={index} {...content.data} />;
+                return <ExperienceSection key={index} {...content.data} />
             }
             if(content.templateId === 'skillsTemplate') {
-                return <SkillSection key={index} {...content.data} />;
+                return <SkillSection key={index} {...content.data} />
             }
             if(content.templateId === 'educationTemplate') {
-                return <CertificationSection key={index} {...content.data} />;
+                return <CertificationSection key={index} {...content.data} />
             }
             if(content.templateId === 'interestsTemplate') {
-                return <InterestSection key={index} {...content.data} />;
+                return <InterestSection key={index} {...content.data} />
             }
             if(content.templateId === 'projectsTemplate') {
-                return <ProjectSection key={index} {...content.data} />;
+                return <ProjectSection key={index} {...content.data} />
             }
-            return <div key={index}></div>;
+            return <div key={index}></div>
         });
     }
 }
@@ -102,14 +102,14 @@ class HeaderSection extends React.Component {
     render() {
         return (
             <div>
-                <div className={this.props.margins}>
+              <div className={this.props.margins}>
                 <div className="flex">
-	                <div className="flex-auto font-bold whitespace-nowrap">{this.props.title}</div>
-	                <div className="flex-auto w-full ml-2">
-	                  <div className="flex flex-col">
-	                    <div className={`h-0 mt-3 border-b border-${this.props.color}`}></div>
-	                  </div>
-	                </div>
+	          <div className="flex-auto font-bold whitespace-nowrap">{this.props.title}</div>
+	          <div className="flex-auto w-full ml-2">
+	            <div className="flex flex-col">
+	              <div className={`h-0 mt-3 border-b border-${this.props.color}`}></div>
+	            </div>
+	          </div>
                 </div>
               </div>
             </div>
@@ -146,18 +146,18 @@ class ContactSection extends React.Component {
             <div>
               <div className="flex mt-4 ml-4">
                 <ul className="list-none text-sm">
-	                <li>
-	                  <i className="fa-solid fa-phone text-accent"></i>
-	                  <span className="ml-2">{this.props.phone}</span>
-	                </li>
-	                <li className="mt-2">
-	                  <i className="fa-solid fa-envelope text-accent"></i>
-	                  <a className="ml-2" href={`mailto:${this.props.email}`}>{this.props.email}</a>
-	                </li>
-	                <li className="mt-2">
-	                  <i className="fa-solid fa-location-dot text-accent"></i>
-	                  <span className="ml-2">{this.props.location}</span>
-	                </li>
+	          <li>
+	            <i className="fa-solid fa-phone text-accent"></i>
+	            <span className="ml-2">{this.props.phone}</span>
+	          </li>
+	          <li className="mt-2">
+	            <i className="fa-solid fa-envelope text-accent"></i>
+	            <a className="ml-2" href={`mailto:${this.props.email}`}>{this.props.email}</a>
+	          </li>
+	          <li className="mt-2">
+	            <i className="fa-solid fa-location-dot text-accent"></i>
+	            <span className="ml-2">{this.props.location}</span>
+	          </li>
                 </ul>
               </div>
             </div>
@@ -192,14 +192,14 @@ class SocialListItem extends React.Component {
     render() {
         return (
             <li className="mt-3">
-	            <div>
-	              <i className={`${this.props.icon} text-accent`}></i>
-	              <span className="ml-2">{this.props.name}:</span>
-	            </div>
-	            <div>
-	              <a className="text-xs" href={this.props.url}>{this.props.url}</a>
-	            </div>
-	          </li>
+	      <div>
+	        <i className={`${this.props.icon} text-accent`}></i>
+	        <span className="ml-2">{this.props.name}:</span>
+	      </div>
+	      <div>
+	        <a className="text-xs" href={this.props.url}>{this.props.url}</a>
+	      </div>
+	    </li>
         );
     }
 }
@@ -226,15 +226,15 @@ class ExperienceListItem extends React.Component {
     render() {
         return (
             <div className="flex flex-col w-full mt-5 text-xs">
-	            <div className="flex justify-between">
-	              <div className="font-bold text-accent">{this.props.title}</div>
-	              <div>{this.props.duration}</div>
-	            </div>
-	            <div className="flex justify-between">
-	              <div>{this.props.name}</div>
-	              <div>{this.props.location}</div>
-	            </div>
-	            <ExperienceDescriptionList descriptions={this.props.descriptions} />
+	      <div className="flex justify-between">
+	        <div className="font-bold text-accent">{this.props.title}</div>
+	        <div>{this.props.duration}</div>
+	      </div>
+	      <div className="flex justify-between">
+	        <div>{this.props.name}</div>
+	        <div>{this.props.location}</div>
+	      </div>
+	      <ExperienceDescriptionList descriptions={this.props.descriptions} />
               <TechnologyList technologies={this.props.technologies} />
             </div>
         );
@@ -247,7 +247,7 @@ class ExperienceDescriptionList extends React.Component {
         return (
             <ul className="fa-ul mt-3 ml-5">
               {descriptionItems}
-	          </ul>
+	    </ul>
         );
     }
 }
@@ -256,11 +256,11 @@ class ExperienceDescriptionItem extends React.Component {
     render() {
         return (
             <li className="mb-1">
-		          <span className="fa-li text-3xs text-primary-dark">
-		            <i className="fa-solid fa-chevron-right"></i>
-		          </span>
-	  	        <span className="text-xs">{this.props.description}</span>
-	          </li>
+              <span className="fa-li text-3xs text-primary-dark">
+	        <i className="fa-solid fa-chevron-right"></i>
+              </span>
+	      <span className="text-xs">{this.props.description}</span>
+	    </li>
         );
     }
 }
@@ -271,7 +271,7 @@ class TechnologyList extends React.Component {
         return (
             <div className="flex flex-wrap mt-1 ml-4">
               {technologyItems}
-	          </div>
+            </div>
         );
     }
 }
@@ -280,8 +280,8 @@ class TechnologyListItem extends React.Component {
     render() {
         return (
             <div className="mt-1 mr-1">
-		          <div className="rounded-md py-0.5 px-1.5 bg-primary-dark text-white">{this.props.technology}</div>
-	          </div>
+	      <div className="rounded-md py-0.5 px-1.5 bg-primary-dark text-white">{this.props.technology}</div>
+	    </div>
         );
     }
 }
@@ -305,7 +305,7 @@ class SkillListItem extends React.Component {
         const skillValueItems = this.props.items.map((item, index) => <SkillValueItem key={index} showGauge={this.props.showGauge} {...item} />);
         return (
             <div>
-	            <div className="mb-2 font-semibold underline">{this.props.name}</div>
+	      <div className="mb-2 font-semibold underline">{this.props.name}</div>
               <div className="grid grid-cols-2 gap-x-16 place-content-between">
                 {skillValueItems}
               </div>
@@ -318,9 +318,9 @@ class SkillValueItem extends React.Component {
     render() {
         return (
             <div className={`grid ${this.props.showGauge ? 'grid-cols-2' : ''} place-content-between`}>
-	              <div>{this.props.name}</div>
-                {this.props.showGauge && <SkillValueGaugeList value={this.props.value} />}
-	          </div>
+	      <div>{this.props.name}</div>
+              {this.props.showGauge && <SkillValueGaugeList value={this.props.value} />}
+	    </div>
         );
     }
 }
@@ -330,8 +330,8 @@ class SkillValueGaugeList extends React.Component {
               const gaugeListItems = [1,2,3,4,5].map((value, index) => <SkillValueGaugeListItem key={index} highlighted={this.props.value >= value} />);
         return (
             <div className="grid grid-cols-5 w-24 content-center">
-	            {gaugeListItems}
-	          </div>
+	      {gaugeListItems}
+	    </div>
         );
     }
 }
@@ -339,7 +339,7 @@ class SkillValueGaugeList extends React.Component {
 class SkillValueGaugeListItem extends React.Component {
     render() {
         return (
-             <div className={`w-2 h-2 ${this.props.highlighted ? 'bg-accent' : 'bg-gray-300'}`}></div>
+            <div className={`w-2 h-2 ${this.props.highlighted ? 'bg-accent' : 'bg-gray-300'}`}></div>
         );
     }
 }
@@ -349,7 +349,7 @@ class CertificationSection extends React.Component {
         const certificationItems = this.props.certifications.map((certification, index) => <Certification key={index} {...certification} />);
         return (
             <div>
-                {certificationItems}
+              {certificationItems}
             </div>
         );
     }
@@ -360,19 +360,19 @@ class Certification extends React.Component {
         return (
             <div className="flex flex-col mt-6">
               <div className="flex">
-	              <div className="flex-auto w-full">
-	                <div className="text-xs">
-	                  <div className="flex justify-between">
-	                    <div className="font-bold text-accent">{this.props.title}</div>
-	                    <div>{this.props.year}</div>
-	                  </div>
-	                  <div className="flex justify-between">
-	                    <div>{this.props.name}</div>
-	                 </div>
-	               </div>
-	             </div>
-             </div>
-           </div>
+	        <div className="flex-auto w-full">
+	          <div className="text-xs">
+	            <div className="flex justify-between">
+	              <div className="font-bold text-accent">{this.props.title}</div>
+	              <div>{this.props.year}</div>
+	            </div>
+	            <div className="flex justify-between">
+	              <div>{this.props.name}</div>
+	            </div>
+	          </div>
+	        </div>
+              </div>
+            </div>
         );
     }
 }
@@ -394,11 +394,11 @@ class Interest extends React.Component {
     render() {
         return (
             <li className="mb-1">
-	            <span className="fa-li text-3xs text-primary-dark">
-	              <i className="fa-solid fa-chevron-right"></i>
-	            </span>
-	            <span className="text-xs">{this.props.description}</span>
-	          </li>
+	      <span className="fa-li text-3xs text-primary-dark">
+	        <i className="fa-solid fa-chevron-right"></i>
+	      </span>
+	      <span className="text-xs">{this.props.description}</span>
+	    </li>
         );
     }
 }
@@ -420,8 +420,8 @@ class Project extends React.Component {
     render() {
         return (
             <div className="flex flex-col w-full mt-5 text-xs">
-	            <div className="font-bold text-accent mr-2">{this.props.name}</div>
-	            <div className="mt-2 ml-1">{this.props.description}</div>
+	      <div className="font-bold text-accent mr-2">{this.props.name}</div>
+	      <div className="mt-2 ml-1">{this.props.description}</div>
               <TechnologyList technologies={this.props.technologies} />
             </div>
         );
