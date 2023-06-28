3172204514a65758a9b94d54b11d270614bd1146
Josh McHugh <joshua.mchugh01@gmail.com>
2023-06-28 00:21:32 -0400

    Renamed files to PascalCase to fit most conventions

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,6 @@
 import { describe, beforeEach, it, expect, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import App from './app';
+import App from './App';
 import data from './../demo-data';
 
 global.fetch = vi.fn();
