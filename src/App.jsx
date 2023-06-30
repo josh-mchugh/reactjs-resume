fae2176f2df40f39d83a1a4612ae0a346f931857
Josh McHugh <joshua.mchugh01@gmail.com>
2023-06-29 21:40:40 -0400

    Refactored ExperienceSection to a seperate module and added unit tests

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import CertificationSection from './CertificationSection';
 import InterestSection from './InterestSection';
 import ProjectSection from './ProjectSection';
 import SkillSection from './SkillSection';
-import TechnologyList from './TechnologyList';
+import ExperienceSection from './ExperienceSection';
 
 class App extends React.Component {
 
@@ -173,48 +173,6 @@ const Social = (props) => {
           <div>
             <a className="text-xs" href={props.url}>{props.url}</a>
           </div>
-        </li>
-    );
-};
-
-const ExperienceSection = (props) => {
-    const experiences = props.experiences.map((content, index) => <Experience key={index} {...content} />);
-    return (
-        <div>
-          <div className="[&>*:first-child]:mt-4">
-            {experiences}
-          </div>
-        </div>
-    );
-};
-
-const Experience = (props) => {
-    const descriptions = props.descriptions.map((description, index) => <ExperienceDescription key={index} description={description} />);
-    return (
-        <div className="flex flex-col w-full mt-5 text-xs">
-          <div className="flex justify-between">
-            <div className="font-bold text-accent">{props.title}</div>
-            <div>{props.duration}</div>
-          </div>
-          <div className="flex justify-between">
-            <div>{props.name}</div>
-            <div>{props.location}</div>
-          </div>
-          <ul className="fa-ul mt-3 ml-5">
-            {descriptions}
-          </ul>
-          <TechnologyList technologies={props.technologies} />
-        </div>
-    );
-};
-
-const ExperienceDescription = (props) => {
-    return (
-        <li className="mb-1">
-          <span className="fa-li text-3xs text-primary-dark">
-            <i className="fa-solid fa-chevron-right"></i>
-          </span>
-          <span className="text-xs">{props.description}</span>
         </li>
     );
 };
---
+++ src/ExperienceSection.test.jsx
@@ -0,0 +1,71 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import ExperienceSection from './ExperienceSection';
+
+describe("ExperienceSection Component", () => {
+
+    const experiences = [
+        {
+            name: "Branded Agency",
+            title: "Web Designer",
+            duration: "Mar 2020 - Jan 2023",
+            location: "Boston, MA",
+            descriptions: [
+                "Creating corporate logos over multiple iterations",
+                "Fixing missized and misused emailed images"
+            ],
+            technologies: [
+                "Photoshop",
+                "Figma"
+            ]
+        }
+    ];
+
+    it("experiance name should be in the document", () => {
+        render(<ExperienceSection experiences={experiences}/>);
+        const element = screen.getByText("Branded Agency");
+        expect(element).toBeInTheDocument();
+    });
+
+    it("experience title should be in the document", () => {
+        render(<ExperienceSection experiences={experiences}/>);
+        const element = screen.getByText("Web Designer");
+        expect(element).toBeInTheDocument();
+    });
+
+    it("experience duration should be in the document", () => {
+        render(<ExperienceSection experiences={experiences}/>);
+        const element = screen.getByText("Mar 2020 - Jan 2023");
+        expect(element).toBeInTheDocument();
+    });
+
+    it("experience location should be in the document", () => {
+        render(<ExperienceSection experiences={experiences}/>);
+        const element = screen.getByText("Boston, MA");
+        expect(element).toBeInTheDocument();
+    });
+
+    it("experience 'creating' description should be in the document", () => {
+        render(<ExperienceSection experiences={experiences}/>);
+        const element = screen.getByText("Creating corporate logos over multiple iterations");
+        expect(element).toBeInTheDocument();
+    });
+
+    it("experience 'fixing' description should be in the document", () => {
+        render(<ExperienceSection experiences={experiences}/>);
+        const element = screen.getByText("Fixing missized and misused emailed images");
+        expect(element).toBeInTheDocument();
+    });
+
+    it("experience 'photoshop' technology should be in the document", () => {
+        render(<ExperienceSection experiences={experiences}/>);
+        const element = screen.getByText("Photoshop");
+        expect(element).toBeInTheDocument();
+    });
+
+    it("experience 'Figma' technology should be in the document", () => {
+        render(<ExperienceSection experiences={experiences}/>);
+        const element = screen.getByText("Figma");
+        expect(element).toBeInTheDocument();
+    })
+});
