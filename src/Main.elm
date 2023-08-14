c20665cb10be9448a30ec61a8e0fba3ec98e6d09
Josh McHugh <joshua.mchugh01@gmail.com>
2023-08-13 23:17:01 -0400

    Add Elm to project

---
+++ src/Main.elm
@@ -0,0 +1,38 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (..)
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+-- Model
+type alias Model =
+    String
+
+init : () -> ( Model, Cmd Msg )
+init () =
+    ( "Hello, Elm here", Cmd.none )
+
+-- Update
+type Msg =
+    NoOp
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    (model, Cmd.none)
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+-- View
+view : Model -> Html Msg
+view model =
+    text model
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { Elm } from './Main.elm';
 import './index.css';
+
+Elm.Main.init({
+    node: document.getElementById('elmRoot')
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
