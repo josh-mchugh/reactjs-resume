3a4820921b71be26a7d2fd3e153982ba862e96b9
Josh McHugh <joshua.mchugh01@gmail.com>
2023-08-13 23:20:23 -0400

    Add Elm to project

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
+import elmPlugin from 'vite-plugin-elm';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        elmPlugin.plugin()
+    ],
     test: {
       globals: true,
       environment: "jsdom",
