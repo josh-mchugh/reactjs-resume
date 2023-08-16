716d9fe2f57940f70ab33f7897fd6fb7c59d4b7d
Josh McHugh <joshua.mchugh01@gmail.com>
2023-08-16 00:26:09 -0400

    Refactored HTML into elm app

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (..)
+import Html.Attributes exposing (class, for, id, placeholder, type_)
 
 
 main : Program () Model Msg
@@ -51,4 +52,14 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    text model
+    div [ class "input__section" ]
+        [ div [ class "section__header" ] [ text "Your Details" ]
+        , label [ for "firstName" ] [ text "First Name" ]
+        , input [ id "firstName", type_ "text", placeholder "First Name" ] []
+        , label [ for "lastName" ] [ text "Last Name" ]
+        , input [ id "lastName", type_ "text", placeholder "Last Name" ] []
+        , label [ for "jobTitle" ] [ text "Job Title" ]
+        , input [ id "jobTitle", type_ "text", placeholder "Job Title " ] []
+        , label [ for "summary" ] [ text "Summary" ]
+        , input [ id "summary", type_ "text", placeholder "Summary" ] []
+        ]
