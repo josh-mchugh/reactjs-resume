4a24310d39e8f8a8d3b1da39c9b8e876d8b4866d
Josh McHugh <joshua.mchugh01@gmail.com>
2023-08-16 23:25:42 -0400

    Added firstName to model and added Html Event listener to the field

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,8 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (class, for, id, placeholder, type_)
+import Html.Attributes exposing (class, for, id, placeholder, type_, value)
+import Html.Events exposing(onInput)
 
 
 main : Program () Model Msg
@@ -20,12 +21,12 @@ main =
 
 
 type alias Model =
-    String
+    { firstName: String }
 
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( "Hello, Elm here", Cmd.none )
+    ( { firstName = "" }, Cmd.none )
 
 
 
@@ -33,12 +34,14 @@ init () =
 
 
 type Msg
-    = NoOp
+    = FirstName String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        FirstName firstName ->
+            ( { model | firstName = firstName }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -55,7 +58,12 @@ view model =
     div [ class "input__section" ]
         [ div [ class "section__header" ] [ text "Your Details" ]
         , label [ for "firstName" ] [ text "First Name" ]
-        , input [ id "firstName", type_ "text", placeholder "First Name" ] []
+        , input [ id "firstName"
+                , type_ "text"
+                , placeholder "First Name"
+                , value model.firstName
+                , onInput FirstName
+                ] []
         , label [ for "lastName" ] [ text "Last Name" ]
         , input [ id "lastName", type_ "text", placeholder "Last Name" ] []
         , label [ for "jobTitle" ] [ text "Job Title" ]
