89d85272d65607ee09f79d8497c617f1aa594419
Josh McHugh <joshua.mchugh01@gmail.com>
2023-09-03 22:05:42 -0400

    Expirmental multipage

--- src/SheetList.jsx
+++ src/SheetList.jsx
@@ -1,34 +1,107 @@
-import  { useEffect, useRef } from 'react';
+import  { useEffect, useRef, useState } from 'react';
 import SectionFactory from './SectionFactory';
 
 const SheetList = (props) => {
-    return <Sheet displayData={props.displayData}/>;
+
+    const [pageBreakIndexes, setPageBreakIndexes] = useState(() => new Set());
+
+    const pageBreakNotifier = row => column => section => {
+        console.log("Page break notifier called: " + [row, column, section].join(","));
+        setPageBreakIndexes((prev) => {
+            const next = new Set(prev);
+            next.add([row, column, section].join(","));
+            return next;
+        });
+    };
+
+    // Create initial sheets array
+    let sheets = [];
+
+    // Create initial rows array
+    let rows = [];
+
+    // Iterate over rows
+    for(let i = 0; i < props.displayData.rows.length; i++) {
+        const row = props.displayData.rows[i];
+
+        // Iterate over columns
+        let columns = [];
+        for(let j = 0; j < row.columns.length; j++) {
+            const column = row.columns[j];
+
+            // Iterate over sections
+            let sections = [];
+            for(let k = 0; k < column.sections.length; k++) {
+                const section = column.sections[k];
+                
+                if(Array.from(pageBreakIndexes).includes([i,j,k].join(","))) {
+                    // Add current rendering process to sheets
+                    const renderColumn = (<RenderColumn key={j} classes={column.class} children={sections} pageBreakNotifier={pageBreakNotifier(i)(j)}/>);
+                    columns = columns.concat(renderColumn);
+
+                    const renderRow = ( <RenderRow key={i} children={columns}/> );
+                    rows = rows.concat(renderRow);
+
+                    const sheet = (<Sheet children={rows}/>);
+                    sheets = sheets.concat(sheet);
+
+                    // Reset sections
+                    sections = [];
+                    columns = [];
+
+                    // Reset columns
+                    row.columns.forEach((column, index) => {
+                        if(index < j) {
+                            const renderColumn = (<RenderColumn key={index} classes={column.class} pageBreakNotifier={pageBreakNotifier(i)(index)}/>);
+                            columns = columns.concat(renderColumn);
+                        }
+                    });
+
+                    // Reest Rows
+                    rows = [...Array(i).keys()].map(index => {
+                        return (<RenderRow key={index} children={columns}/>);
+                    });
+                }
+
+                const renderSection = (<SectionFactory key={k} section={section} />);
+                sections = sections.concat(renderSection);
+            }
+
+            const renderColumn = (<RenderColumn key={j} classes={column.class} children={sections} pageBreakNotifier={pageBreakNotifier(i)(j)}/>);
+            columns = columns.concat(renderColumn);
+        }
+
+        const renderRow = ( <RenderRow key={i} children={columns} />);
+        rows = rows.concat(renderRow);
+    }
+
+    sheets = sheets.concat( (<Sheet children={rows} />) );
+
+    return sheets;
 };
 
 const Sheet = (props) => {
-    const rows = props.displayData.rows.map((row, index) => {
-        return <RenderRow key={index} row={row} />;
-    });
 
     return (
         <section className="sheet">
           <div className="flex h-full font-['Roboto'] text-gray-700">
-            { rows }
+            { props.children }
           </div>
         </section>
     );
 };
 
 const RenderRow = (props) => {
-    return props.row.columns.map((column, index) =>
-        <RenderColumn key={index} column={column} />
+    return (
+        <>
+          { props.children }
+        </>
     );
 };
 
 const RenderColumn = (props) => {
     const contentRef = useRef(0);
     let contentHeight = 0;
-    let currentIndex = 0;
 
     useEffect(() => {
         if(contentRef && contentRef.current) {
@@ -36,18 +109,15 @@ const RenderColumn = (props) => {
 
             if(contentHeight > 920) {
                 console.log("content exceeds 920");
+                props.pageBreakNotifier(props.children.length - 1);
             }
         }
     }, [props]);
 
     return (
-        <div className={`${props.column.class}`}>
+        <div className={`${props.classes}`}>
           <div style={{height: "auto"}} ref={contentRef}>
-            { props.column.sections.map((section, index) => {
-                currentIndex = index;
-                return <SectionFactory key={index} section={section} />;
-            })
-            }
+            { props.children }
           </div>
         </div>
     );
